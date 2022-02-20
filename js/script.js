a457e040aadb2c35dfc7e7d27491f5267c46c0a9
VAJRESH <vajresh005@gmail.com>
2022-02-20 23:56:55 +0530

    Feat: Add Details
    Added details
    Navbar fixed
    Images are pending and customers

--- js/script.js
+++ js/script.js
@@ -1,4 +1,48 @@
+const customers = [
+  {
+    id: 1,
+    name: "Rosa Developers",
+    img: "../images/rosa-developers.png",
+  },
+  {
+    id: 2,
+    name: "Pyramid Developers",
+    img: "../images/pyramid-developers.png",
+  },
+  {
+    id: 3,
+    name: "Aurum IT Park",
+    img: "../images/aurum-it-ark.png",
+  },
+  {
+    id: 4,
+    name: "Paraduse Developers",
+    img: "../images/paraduse-developers.png",
+  },
+  {
+    id: 5,
+    name: "Metro Developers",
+    img: "../images/metro-developers.png",
+  },
+  {
+    id: 6,
+    name: "GAMI Developers",
+    img: "../images/gami-developers.png",
+  },
+  {
+    id: 7,
+    name: "Juhi Developers",
+    img: "../images/juhi-developers.png",
+  },
+  {
+    id: 8,
+    name: "Nahar Builders",
+    img: "../images/nahar-builders.png",
+  },
+];
 $(document).ready(function () {
+  loadCustomers();
+
   $(window).scroll(function () {
     // sticky navbar on scroll script
     if (this.scrollY > 20) {
@@ -32,115 +76,28 @@ $(document).ready(function () {
     $(".navbar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
   });
-
-  // reviews
-  const reviews = [
-    {
-      id: 1,
-      name: "susan smith",
-      job: "web developer",
-      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-      text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-    },
-    {
-      id: 2,
-      name: "anna johnson",
-      job: "web designer",
-      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-      text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-    },
-    {
-      id: 3,
-      name: "peter jones",
-      job: "intern",
-      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-      text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-    },
-    {
-      id: 4,
-      name: "bill anderson",
-      job: "the boss",
-      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-      text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    },
-  ];
-  // select items
-  const img = document.getElementById("person-img");
-  const author = document.getElementById("author");
-  const job = document.getElementById("job");
-  const info = document.getElementById("info");
-
-  const prevBtn = document.querySelector(".prev-btn");
-  const nextBtn = document.querySelector(".next-btn");
-  const randomBtn = document.querySelector(".random-btn");
-
-  // set starting item
-  let currentItem = 0;
-
-  // load initial item
-  window.addEventListener("DOMContentLoaded", function () {
-    const item = reviews[currentItem];
-    img.src = item.img;
-    author.textContent = item.name;
-    job.textContent = item.job;
-    info.textContent = item.text;
-  });
-
-  // show person based on item
-  function showPerson(person) {
-    const item = reviews[person];
-    img.src = item.img;
-    author.textContent = item.name;
-    job.textContent = item.job;
-    info.textContent = item.text;
-  }
-  // show next person
-  nextBtn.addEventListener("click", function () {
-    currentItem++;
-    if (currentItem > reviews.length - 1) {
-      currentItem = 0;
-    }
-    showPerson(currentItem);
-  });
-  // show prev person
-  prevBtn.addEventListener("click", function () {
-    currentItem--;
-    if (currentItem < 0) {
-      currentItem = reviews.length - 1;
-    }
-    showPerson(currentItem);
-  });
-  // show random person
-  randomBtn.addEventListener("click", function () {
-    console.log("hello");
-
-    currentItem = Math.floor(Math.random() * reviews.length);
-    showPerson(currentItem);
-  });
-
-  // owl carousel script
-  $(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      600: {
-        items: 2,
-        nav: false,
-      },
-      1000: {
-        items: 3,
-        nav: false,
-      },
-    },
-  });
 });
+
+function getHTMLForCustomer(customerObj) {
+  return `
+  <div class='customer-card'>
+    <div class="img-container">
+      <img src="${customerObj.img}" alt="${customerObj.name}"/>
+    </div>
+    <p>${customerObj.name}</p>
+  </div>
+    `;
+}
+
+function loadCustomers() {
+  let html = "";
+  customers.forEach((customer) => {
+    html += getHTMLForCustomer(customer);
+  });
+  html += "";
+
+  $(".customer-carousel").html(html);
+}
 
 // send email
 $("#emailForm").submit(function (e) {
